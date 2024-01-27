ada3bd59752d9e02116733030a3edb8a11da00fb
Pratyush Goutam <113873593+prat555@users.noreply.github.com>
2024-01-27 18:12:32 +0530

    Update app.js

--- Blog Website/app.js
+++ Blog Website/app.js
@@ -2,21 +2,24 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const port=3000;
+
 app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded(( extended: true )));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public'));
+                       
 let posts = [];
+
 app.get("/", (req, res) => (
-res.render( index', {posts});
+  res.render( index', {posts});
 });
 
-app.post('/post', (req, res) => (
-const title, content . req.k body;
-const newPost = f title, content );
-posts.push(newPost);
-res.redirect("/');
+app.post('/post', (req, res) => {
+  const { title, content } = req.body;
+  const newPost = { title, content };
+  posts.push(newPost);
+  res.redirect("/');
 });
 
 app.listen(port, () => {
-console.log(`Server is running on http://localhost:$(port)");
+console.log('Server is running on http://localhost:${port}');
 });
